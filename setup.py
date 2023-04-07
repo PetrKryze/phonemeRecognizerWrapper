6eba43dfc8d46a347da5a15c82786e358df0cbc9
Petr Krýže <petr.kryze@gmail.com>
2023-04-07 14:54:45 +0200

    Updated the build system.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='phonemeRecognizerWrapper',
 
     # Version number (initial):
-    version='0.1.3',
+    version='0.1.4',
 
     # Application author details:
     author='Petr Krýže',
